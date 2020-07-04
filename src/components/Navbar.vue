252ede72d52c5d32744c175c9c51c6c98d98232a
NabeelahY <noblenab13@gmail.com>
2020-07-04 14:50:39 +0100

    Create navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav v-for="(link, index) in links" :key="index">
+        <b-nav-item href="#">{{ link }}</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      links: ["Home", "Team", "Services", "Contact"]
+    };
+  }
+};
+</script>
