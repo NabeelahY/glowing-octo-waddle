062f05fc592dbcda6702cfec51607ffe382a0708
NabeelahY <noblenab13@gmail.com>
2020-07-04 15:29:26 +0100

    Add hero component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <Navbar />
+    <Hero />
+    <hr />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Hero
   }
 }
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-container>
+    <b-row class="text-center hero">
+      <b-col cols="6">
+        <b-img fluid :src="image" alt="Image 1"></b-img>
+      </b-col>
+      <b-col class="mid">
+        <h1>Welcome to Site!</h1>
+        <p>Leading market for all things luck</p>
+        <b-button variant="info" href="#">More Info</b-button>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import image from "../assets/hero.svg";
+export default {
+  name: "Hero",
+  data() {
+    return {
+      links: ["Home", "Team", "Services", "Contact"],
+      image: image
+    };
+  }
+};
+</script>
+
+<style scoped>
+.hero {
+  padding: 30px 0;
+}
+.mid {
+  flex-direction: column;
+  align-self: center;
+}
+</style>
